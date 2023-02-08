4486ca1206eba0977e1db3a1060e3bd4c8756911
KuraoHikari <dewaindra705@gmail.com>
2023-02-08 20:39:55 +0800

    update java dasar

---
+++ src/TipeDataChar.java
@@ -0,0 +1,11 @@
+public class TipeDataChar {
+    public static void main(String[] args) {
+
+        char k = 'K';
+        char h = 'H';
+
+        System.out.print(k);
+        System.out.print(h);
+
+    }
+}
---
+++ src/Variable.java
@@ -0,0 +1,23 @@
+public class Variable {
+    public static void main(String[] args) {
+        String name;
+        name = "kurao Hikari";
+
+        System.out.println(name);
+
+        int age = 22;
+        String address = "Indonesia";
+
+        System.out.println(age);
+        System.out.println(address);
+
+        name = "Indra Putra";
+        System.out.println(name);
+
+        var firstName = "Kurao";
+        var middleName = "Hikari";
+        var lastName = "indra";
+
+        final String application = "Belajar Java";
+    }
+}
